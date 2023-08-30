36bbf1baee81e869c90273fcde64fb3cc06d8969
Isaac <imorris0004@gmail.com>
2023-08-30 06:16:45 -0400

    homework update

--- ch02_isaac_morris.js
+++ ch02_isaac_morris.js
@@ -8,4 +8,64 @@ while (line.length <= 7) {
     line += "#" ;
 }
 
-//
+// "FizzBuzz"
+let num = 0;
+while (num < 100) {
+    num += 1;
+     if (num % 3 == 0 && num % 5 == 0) {
+        console.log("FizzBuzz");
+     } else if (num % 3 == 0) {
+            console.log("Fizz");
+        } else if (num % 5 == 0) {
+            console.log("Buzz");
+        } else {
+            console.log(num);
+}
+}
+
+// chessboard
+function chessboard(size) {
+
+let lineCount = 0;
+
+charOne = "#";
+charTwo = " ";
+sequenceOne = charOne + charTwo
+sequenceTwo = charTwo + charOne
+
+function sOne(size) {
+    if (size & 2 !== 0) {
+        size = size - 1;
+        console.log(sequenceOne.repeat(size/2) + "#");
+    } else {
+        console.log(sequenceOne.repeat(size/2));
+    }
+    }
+    function sTwo(size) {
+        if (size & 2 !== 0) {
+            size = size - 1;
+            console.log(sequenceTwo.repeat(size/2) + " ");
+        } else {
+            console.log(sequenceTwo.repeat(size/2));
+        }
+        }
+
+while (lineCount < size) {
+    sOne(size);
+    lineCount += 1;
+    if (lineCount == size) {
+        break
+    } else {
+    sTwo(size);
+    lineCount += 1;
+    }
+}
+}
+
+chessboard(10)
+
+
+
+
+
+
